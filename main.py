2af830de9b84a8411069a1f429c5ce9b7c4514ad
Dzima-G <dgukalin@gmail.com>
2023-07-01 15:20:32 +0300

    download book covers

--- main.py
+++ main.py
@@ -13,18 +13,19 @@ def get_books(url):
 
 def check_for_redirect(response):
     if response.status_code == 302:
-        print('СРАБОТАЛО')
+        raise requests.HTTPError
+    if len(response.history) == 2:
         raise requests.HTTPError
 
 
-def get_book_name(book_id):
-    url = f'https://tululu.org/b{book_id}'
+def get_book_name(url):
     response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
     soup = BeautifulSoup(response.text, 'lxml')
-    title_tag = soup.find('h1')
-    title_text = title_tag.text.split('   ::   ')
+    image_url = f"https://tululu.org/{soup.find('div', class_='bookimage').find('img')['src']}"
+    title_text = soup.find('h1').text.split('   ::   ')
+    title_text.append(image_url)
     return title_text
 
 
@@ -47,16 +48,32 @@ def download_txt(url, filename, folder='books/'):
     return file_path
 
 
+def download_image(url, folder='images/'):
+    fpath = sanitize_filepath(folder)
+    os.makedirs(fpath, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    filename = sanitize_filename(url.split('/')[-1])
+    file_path = os.path.join(folder, filename)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+
 if __name__ == "__main__":
     books_folder_name = 'books'
     url = 'https://tululu.org/'
     i = 1
     for book_id in range(1, 11):
         book_url = f'{url}/txt.php?id={book_id}'
+        page_url = f'{url}/b{book_id}'
         try:
-            book_name = f'{i}. {get_book_name(book_id)[0]}'
+            book_poster = get_book_name(page_url)
+            book_name = f'{i}. {book_poster[0]}'
             download_txt(book_url, book_name)
+            download_image(book_poster[2])
         except requests.HTTPError:
-            print("Книга отсутствует с id ==", book_id)
+            # print("Книга отсутствует с id ==", book_id)
             continue
+        print(f'Заголовок:{book_name}', book_poster[2], '', sep='\n', end='\n')
         i += 1
